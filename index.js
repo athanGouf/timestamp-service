9eb60261d817c62c2ce39bef80e116ea9f9df6e9
Thanasis <athan.gouf@gmail.com>
2019-03-24 14:02:47 +0200

    example route created

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello node");
+const app = require('express')();
+
+app.get('/', (req,res) => {
+    res.send('Timestamp microservice is working!');
+});
+app.listen('8000', () => {
+    console.log('listening port 8000');
+});
