854f48565e35c8bc5539a6311e4cc6dc923d823e
Thanasis <athan.gouf@gmail.com>
2019-03-24 14:43:19 +0200

    install nodemon to reload server automatically

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('express')();
 app.get('/', (req,res) => {
     res.send('Timestamp microservice is working!');
 });
+
 app.listen('8000', () => {
     console.log('listening port 8000');
 });
